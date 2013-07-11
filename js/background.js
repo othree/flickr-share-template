fda3e3f447177c58712f74377348cd34bde22bee
othree_kao <othree_kao@htc.com>
2013-07-11 10:54:13 +0800

    jslint background

--- js/background.js
+++ js/background.js
@@ -1,16 +1,19 @@
+/*jslint vars: true, browser: true */
+/*global chrome: false */
 (function () {
+    "use strict";
+
     var showPageAction = function (tabId, changeInfo, tab) {
-            var url = tab.url,
-                urlobj = document.createElement('a');
+        var url = tab.url,
+            urlobj = document.createElement('a');
 
-            urlobj.href = url;
+        urlobj.href = url;
 
-            var frags = urlobj.pathname.split('/');
+        var frags = urlobj.pathname.split('/');
 
-            if (urlobj.hostname === 'www.flickr.com' && frags.length >= 4 && frags[1] === 'photos' && /^\d+$/.test(frags[3])) {
-		        chrome.pageAction.show(tabId);
-            }
-
+        if (urlobj.hostname === 'www.flickr.com' && frags.length >= 4 && frags[1] === 'photos' && /^\d+$/.test(frags[3])) {
+            chrome.pageAction.show(tabId);
+        }
     };
 
 	chrome.tabs.onUpdated.addListener(showPageAction);
