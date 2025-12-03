ff0577c2946c8a026f36415220e045a62486da82
othree <othree@gmail.com>
2025-12-03 11:16:27 +0900

    feat: supports domain w/o www

--- js/background.js
+++ js/background.js
@@ -1,30 +1,36 @@
 /*jslint vars: true, browser: true */
 /*global chrome: false */
 (function () {
-    "use strict";
+  "use strict";
 
-    var showAction = function (tabId, changeInfo, tab) {
-        var url = tab.url;
+  var showAction = function (tabId, changeInfo, tab) {
+    var url = tab.url;
 
-        if (!url) {
-            return;
-        }
+    if (!url) {
+      return;
+    }
 
-        var urlobj;
-        try {
-            urlobj = new URL(url);
-        } catch (e) {
-            return;
-        }
+    var urlobj;
+    try {
+      urlobj = new URL(url);
+    } catch (e) {
+      return;
+    }
 
-        var frags = urlobj.pathname.split('/');
+    var frags = urlobj.pathname.split("/");
 
-        if (urlobj.hostname === 'www.flickr.com' && frags.length >= 4 && frags[1] === 'photos' && /^\d+$/.test(frags[3])) {
-            chrome.action.enable(tabId);
-        } else {
-            chrome.action.disable(tabId);
-        }
-    };
+    if (
+      (urlobj.hostname === "www.flickr.com" ||
+        urlobj.hostname === "flickr.com") &&
+      frags.length >= 4 &&
+      frags[1] === "photos" &&
+      /^\d+$/.test(frags[3])
+    ) {
+      chrome.action.enable(tabId);
+    } else {
+      chrome.action.disable(tabId);
+    }
+  };
 
-	chrome.tabs.onUpdated.addListener(showAction);
-}());
+  chrome.tabs.onUpdated.addListener(showAction);
+})();
